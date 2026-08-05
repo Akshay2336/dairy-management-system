cfb5687c4d2f6b0fda2cdb4490f0ea9b973495e4
Akshay2336 <akshaykadam2336@gmail.com>
2026-08-05 08:59:39 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API = axios.create({
 
 //baseURL:"http://localhost:5000/api"
-baseURL:"https://dairy-management-system-wlxo.onrender.com"
+baseURL:"https://dairy-management-system-wlxo.onrender.com/api"
 
 });
 
